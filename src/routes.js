c8de1448eb65b61ed2795216aa802fcc2f3e02e4
Rajvardhan <rajvardhan.s@chapter247.com>
2021-10-26 17:43:26 +0530

    Change password ui complate user list in process

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,16 @@ const Dashboard = React.lazy(() => import('./components/dashboard/Dashboard'));
 const User_dashboard = React.lazy(() => import('./UserSide/Components/user_dashboard/User_dashboard'));
 const Holidays = React.lazy(() => import('./components/holidays/Holidays'));
 const Users = React.lazy(() => import('./components/tables/Tables'));
+const User_profile = React.lazy(() => import('./UserSide/Components/User_profile/User_profile'))
+const Forgot_password = React.lazy(() => import('./UserSide/Components/forget_password/Forgot_password'))
 
 const routes = [
   { path: '/user_dashboard', exact: true , name: 'User_dashboard', component: User_dashboard },
+  { path: '/User_profile', exact: true , name: 'User_profile', component: User_profile },
   { path: '/dashboard', exact: true , name: 'Dashboard', component: Dashboard },
   { path: '/holidays', exact: true , name: 'Holidays', component: Holidays },
   { path: '/users', exact: true , name: 'Users', component: Users },
+  { path: '/Forgot_password', exact: true , name: 'Forgot_password', component: Forgot_password },
 ];
 
 export default routes;
