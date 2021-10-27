09096c05b64fb78dc2baf526a7547e39484e8abe
mohit247 <mohit.b@chapter247.com>
2021-10-27 18:08:34 +0530

    minor changes

--- api/src/routes/leaveRequest.js
+++ api/src/routes/leaveRequest.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import { LeaveRequestController } from '../controllers/apis';
 import { verifyToken } from '../utils';
-import { AddLeaveRequestValidation, updateLeaveStatus } from '../validations';
-const router = express.Router();
-
 import { AddLeaveRequestValidation, UpdateLeaveStatus } from '../validations';
+const router = express.Router();
 router.post(
   '/add',
   verifyToken,
