502e0a3892696efc1f019b48c3324f31269b33d7
mohit247 <mohit.b@chapter247.com>
2021-10-27 15:56:30 +0530

    api doc for leave status

--- api/src/controllers/apis/leaveRequest.js
+++ api/src/controllers/apis/leaveRequest.js
@@ -5,9 +5,40 @@ import { Email, AvailableTemplates } from '../../utils';
 import { Types } from 'mongoose';
 
 /**
- * Create a record
- * @param { req, res }
- * @returns JsonResponse
+ -----------------------
+    ADD LEAVE REQUEST
+ -----------------------
+ */
+/**
+ * @api {post} leave-request/add  Add Leave Request
+ * @apiName addLeaveRequest
+ * @apiGroup LeaveRequest
+ * @apiPermission admin/employee
+ * @apiDescription Add API for Leave Request
+ * @apiParam {String} datesToRequest Requested dates for leave.
+ * @apiParam {String} reason reason  for leave.
+ * @apiParamExample {Object} Request-Example:
+{
+    
+      reason:"fever",
+      datesToRequest:["2021-10-10","2021-10-11"] ,
+}
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+* {
+ *   responseCode: 200,
+      data: result,
+      message: 'Leave Request Added successfully.',
+      success: true,
+ * }
+ * @apiErrorExample {json} List error
+
+ *  HTTP/1.1 400 notFound
+ * {
+       message: "Your account is deactivated please contact to admin",
+      success: false,
+ * }
+ *    HTTP/1.1 500 Internal Server Error
  */
 const addLeaveRequest = async (req, res) => {
   try {
@@ -23,7 +54,6 @@ const addLeaveRequest = async (req, res) => {
       return res.status(400).json({
         success: false,
         message: Message.AccountDeactivated,
-        success: false,
       });
     }
     const data = {
@@ -82,9 +112,28 @@ const addLeaveRequest = async (req, res) => {
 };
 
 /**
- * Get only single record
- * @param { req, res }
- * @returns JsonResponse
+ -------------------------
+   GET ALL LEAVE REQUEST
+ -------------------------
+ */
+
+/**
+ * @api {get} leave-request/list  Get Leave Request list
+ * @apiName leaveRequestList
+ * @apiGroup LeaveRequest
+ * @apiPermission admin
+ * @apiDescription To fetch Leave Request details 
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * {
+ *   message: Leave Request list fetched successfully
+ *   responseCode: 200,
+      data,
+      totalRecords,
+      success: true,
+ * }
+ * @apiErrorExample {json}  error
+ *    HTTP/1.1 500 Internal Server Error
  */
 const leaveRequestList = async (req, res, next) => {
   try {
@@ -233,12 +282,33 @@ const leaveRequestList = async (req, res, next) => {
     return res.status(500).json({
       success: false,
       message: Message.UnexpectedError,
-
       error: error.message,
     });
   }
 };
+/**
+ -------------------------
+   GET A LEAVE REQUEST
+ -------------------------
+ */
 
+/**
+ * @api {get} leave-request/view/:id  Get Leave Request view
+ * @apiName leaveRequestView
+ * @apiGroup LeaveRequest
+ * @apiPermission admin
+ * @apiDescription To fetch Leave Request details 
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * {
+ *   message: Leave Request details fetched successfully
+      data,
+      totalRecords,
+      success: true,
+ * }
+ * @apiErrorExample {json}  error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 const leaveRequestView = async (req, res) => {
   try {
     const { params } = req;
@@ -296,7 +366,29 @@ const leaveRequestView = async (req, res) => {
     });
   }
 };
+/**
+ --------------------------------
+     LEAVE REQUEST OF USER
+ --------------------------------
+ */
 
+/**
+ * @api {get} leave-request/by-user/:id  Get Leave Request view of user
+ * @apiName leaveRequestByUser
+ * @apiGroup LeaveRequest
+ * @apiPermission admin/employee
+ * @apiDescription To fetch Leave Request details of user
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * {
+ *   message: Leave Request list fetched successfully
+      data,
+      totalRecords,
+      success: true,
+ * }
+ * @apiErrorExample {json}  error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 const leaveRequestByUser = async (req, res) => {
   try {
     const { params } = req;
@@ -354,6 +446,29 @@ const leaveRequestByUser = async (req, res) => {
     });
   }
 };
+/**
+ --------------------------------
+   LEAVE REQUEST OF CURRENT USER
+ --------------------------------
+ */
+
+/**
+ * @api {get} leave-request/me  Get Leave Request list of current user
+ * @apiName myLeaveRequest
+ * @apiGroup LeaveRequest
+ * @apiPermission admin/employee
+ * @apiDescription To fetch Leave Request details of current user
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * {
+ *   message: Leave Request list fetched successfully
+      data,
+      totalRecords,
+      success: true,
+ * }
+ * @apiErrorExample {json}  error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 const myLeaveRequest = async (req, res) => {
   try {
     const { currentUser } = req;
@@ -411,6 +526,42 @@ const myLeaveRequest = async (req, res) => {
     });
   }
 };
+/**
+ --------------------------------
+    UPDATE LEAVE REQUEST STATUS
+ --------------------------------
+ */
+/**
+ * @api {patch} leave-request/update-status/:id  Update Leave Request status
+ * @apiName updateLeaveStatus
+ * @apiGroup LeaveRequest
+ * @apiPermission admin
+ * @apiDescription Update API for Leave Request status
+ * @apiParam {String} status status to be updated for leave.
+ * @apiParam {String} id id of leave request.
+ * @apiParamExample {Object} Request-Example:
+   {
+    
+      id:"44455522225544ff5",
+      status:"Approved" ,
+   }
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+* {
+ *   responseCode: 200,
+      data: data,
+      message: 'Leave Request Status Updated successfully.',
+      success: true,
+ * }
+ * @apiErrorExample {json} List error
+
+ *  HTTP/1.1 400 notPermitted
+ * {
+       message: "Your are not permitted to update leave status",
+      success: false,
+ * }
+ *    HTTP/1.1 500 Internal Server Error
+ */
 
 const updateLeaveStatus = async (req, res) => {
   try {
