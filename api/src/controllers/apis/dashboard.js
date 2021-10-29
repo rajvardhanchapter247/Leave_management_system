2c5a265f70e195d9f5c67682f95e9acb6f6a033f
mohit247 <mohit.b@chapter247.com>
2021-10-29 15:45:15 +0530

    dashboard api optimise

--- api/src/controllers/apis/dashboard.js
+++ api/src/controllers/apis/dashboard.js
@@ -31,41 +31,151 @@ import constant from '../../common/constant';
  */
 const userAndLeaveCount = async (req, res) => {
   try {
-    const totalUsers = await UserModel.countDocuments({
-      isDeleted: false,
-    });
-    const totalEmployees = await UserModel.countDocuments({
-      isDeleted: false,
-      status: 'Active',
-      role: 'Employee',
-    });
-    const totalAdmin = await UserModel.countDocuments({
-      role: 'Admin',
-      status: 'Active',
-      isDeleted: false,
-    });
-    const pendingLeaves = await LeaveRequestModel.countDocuments({
-      status: 'Pending',
-    });
-    const approvedLeaves = await LeaveRequestModel.countDocuments({
-      status: 'Approved',
-    });
-    const disapprovedLeaves = await LeaveRequestModel.countDocuments({
-      status: 'Disapproved',
-    });
+    const user = await UserModel.aggregate([
+      {
+        $match: {
+          isDeleted: false,
+        },
+      },
+      {
+        $facet: {
+          totalUsers: [
+            {
+              $count: 'count',
+            },
+          ],
+          totalEmployees: [
+            {
+              $match: {
+                status: 'Active',
+                role: 'Employee',
+              },
+            },
+            {
+              $count: 'count',
+            },
+          ],
+          totalAdmin: [
+            {
+              $match: {
+                status: 'Active',
+                role: 'Admin',
+              },
+            },
+            {
+              $count: 'count',
+            },
+          ],
+          totalActiveUser: [
+            {
+              $match: {
+                status: 'Active',
+              },
+            },
+            {
+              $count: 'count',
+            },
+          ],
+          totalUnActiveUser: [
+            {
+              $match: {
+                status: 'Inactive',
+              },
+            },
+            {
+              $count: 'count',
+            },
+          ],
+        },
+      },
+    ]);
+    const leaveRequest = await LeaveRequestModel.aggregate([
+      {
+        $facet: {
+          totalRequestedLeaves: [
+            {
+              $count: 'count',
+            },
+          ],
+          pendingLeaves: [
+            {
+              $match: {
+                status: 'Pending',
+              },
+            },
+            {
+              $count: 'count',
+            },
+          ],
+          approvedLeaves: [
+            {
+              $match: {
+                status: 'Approved',
+              },
+            },
+            {
+              $count: 'count',
+            },
+          ],
+          disapprovedLeaves: [
+            {
+              $match: {
+                status: 'Disapproved',
+              },
+            },
+            {
+              $count: 'count',
+            },
+          ],
+        },
+      },
+    ]);
 
     const paidLeaves = constant.paidLeaves;
     const data = {
-      totalUsers,
-      totalEmployees,
-      totalAdmin,
-      pendingLeaves,
-      approvedLeaves,
-      disapprovedLeaves,
+      totalUsers:
+        user[0].totalUsers && user[0].totalUsers.length
+          ? user[0].totalUsers[0].count
+          : 0,
+      totalEmployees:
+        user[0].totalEmployees && user[0].totalEmployees.length
+          ? user[0].totalEmployees[0].count
+          : 0,
+      totalAdmin:
+        user[0].totalAdmin && user[0].totalAdmin.length
+          ? user[0].totalAdmin[0].count
+          : 0,
+      totalActiveUser:
+        user[0].totalActiveUser && user[0].totalActiveUser.length
+          ? user[0].totalAdmin[0].count
+          : 0,
+      totalUnActiveUser:
+        user[0].totalUnActiveUser && user[0].totalUnActiveUser.length
+          ? user[0].totalUnActiveUser[0].count
+          : 0,
+      totalRequestedLeaves:
+        leaveRequest[0].totalRequestedLeaves &&
+        leaveRequest[0].totalRequestedLeaves.length
+          ? leaveRequest[0].totalRequestedLeaves[0].count
+          : 0,
+      pendingLeaves:
+        leaveRequest[0].pendingLeaves && leaveRequest[0].pendingLeaves.length
+          ? leaveRequest[0].pendingLeaves[0].count
+          : 0,
+      approvedLeaves:
+        leaveRequest[0].approvedLeaves && leaveRequest[0].approvedLeaves.length
+          ? leaveRequest[0].approvedLeaves[0].count
+          : 0,
+      disapprovedLeaves:
+        leaveRequest[0].disapprovedLeaves &&
+        leaveRequest[0].disapprovedLeaves.length
+          ? leaveRequest[0].disapprovedLeaves[0].count
+          : 0,
       paidLeaves,
     };
     return res.status(200).json({
       success: true,
+      data,
       message: Message.ListFetchSuccess.replace(
         ':item',
         'User and Leave Count'
